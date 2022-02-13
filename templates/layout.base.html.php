03cd1904347a08389003a90b0baba703952c4ff3
libasse <libassalaye24@gmail.com>
2022-02-13 22:20:35 +0000

    layout

---
+++ templates/layout.base.html.php
@@ -0,0 +1,10 @@
+<?php
+require_once(PATH_VIEWS_INC."header.inc.html.php");
+require_once(PATH_VIEWS_INC."menu.inc.html.php");
+
+//$content_for_views contient les vues de notre application
+echo $content_for_views;
+
+
+require_once(PATH_VIEWS_INC."footer.inc.html.php");
+?>
