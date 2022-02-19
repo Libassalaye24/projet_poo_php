1f720d803e47f20ccee447a2956925e3ccb8eb31
libasse <libassalaye24@gmail.com>
2022-02-19 01:33:24 +0000

    requete sql select

--- src/Repository/ChambreRepository.php
+++ src/Repository/ChambreRepository.php
@@ -15,8 +15,12 @@ class ChambreRepository extends AbstractRepository
     }
     public function findAll(): array
     {
-        $sql="select * from $this->tableName c,type_chambre t where t.id_type_chambre=c.id_type_chambre ";
-        return $this->database->executeSelect($sql);
+        $sql="select * from $this->tableName c,type_chambre t where t.id_type_chambre=c.id_type_chambre and etat=?";
+        return $this->database->executeSelect($sql,['non_archiver']);
+    }
+    public function findChambreArchiver(string $etat):array{
+        $sql="select * from $this->tableName c,type_chambre t where t.id_type_chambre=c.id_type_chambre and etat=?";
+        return $this->database->executeSelect($sql,[$etat]);
     }
     public function findById(int $id): array
     {
@@ -24,9 +28,14 @@ class ChambreRepository extends AbstractRepository
         return $this->database->executeSelect($sql,[$id]);
 
     }
-    public function findChambrePavillon(int $id):object|bool|array
+    public function findChambreType(int $id):object|bool|array
     {
         $sql="select * from $this->tableName c,type_chambre t where c.id_type_chambre=t.id_type_chambre and c.$this->primaryKey=?";
+        return $this->findBy($sql,[$id]);
+    }
+    public function findChambrePavillon(int $id):object|bool|array
+    {
+        $sql="select * from $this->tableName c,pavillon p where c.id_pavillon=p.id_pavillon and c.$this->primaryKey=?";
         return $this->findBy($sql,[$id]);
     }
     public function findChambrePavillonNull()
@@ -36,7 +45,12 @@ class ChambreRepository extends AbstractRepository
     }
     public function findChambrePavillonNotNull()
     {
-        $sql="select * from $this->tableName where id_pavillon IS NOT NULL ";
-        return $this->database->executeSelect($sql);
+        $sql="select * from $this->tableName where id_pavillon IS NOT NULL and etat=?";
+        return $this->database->executeSelect($sql,['non_archiver']);
+    }
+    public function findChambreByPavillon(int $id)
+    {
+        $sql="select * from $this->tableName c,pavillon p,type_chambre t where t.id_type_chambre=c.id_type_chambre  and c.id_pavillon=p.id_pavillon and c.id_pavillon=?";
+        return $this->database->executeSelect($sql,[$id]);
     }
 }
